969ad6a67aafcb6c52cc3e738ad07e0c8934887b
Zagreba Dev <zagreba.dev@gmail.com>
2023-03-28 13:19:20 +0300

    created class api_client and implemented fetch_movies_list

---
+++ lib/api_clients/api_client.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:af_trial_project/entity/movies_response.dart';
+import 'package:http/http.dart' as http;
+
+class ApiClient {
+  final httpClient = http.Client();
+
+  Future<MoviesResponse> fetchMoviesList([int pageQuery = 0]) async {
+    try {
+      final response = await httpClient.get(
+        Uri.https(
+          'live.mocat.amifactory.network',
+          '/api/v1/movies/',
+          <String, dynamic>{
+            'page': '$pageQuery',
+          },
+        ),
+      );
+      if (response.statusCode == 200) {
+        final body = json.decode(response.body) as Map;
+        return MoviesResponse.fromJson(body as Map<String, dynamic>);
+      }
+      throw Exception('error fetching posts');
+    } catch (e) {
+      print('fetchPosts ERROR IS: $e');
+      throw Exception('error fetching posts');
+    }
+  }
+}

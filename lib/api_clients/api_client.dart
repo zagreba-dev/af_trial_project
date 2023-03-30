8bd12d4bb5d25e88e44882feb6857fc456a424e2
Zagreba Dev <zagreba.dev@gmail.com>
2023-03-30 18:36:41 +0300

    refactored api_client

--- lib/api_clients/api_client.dart
+++ lib/api_clients/api_client.dart
@@ -7,24 +7,19 @@ class ApiClient {
   final httpClient = http.Client();
 
   Future<MoviesResponse> fetchMoviesList([int pageQuery = 0]) async {
-    try {
-      final response = await httpClient.get(
-        Uri.https(
-          'live.mocat.amifactory.network',
-          '/api/v1/movies/',
-          <String, dynamic>{
-            'page': '$pageQuery',
-          },
-        ),
-      );
-      if (response.statusCode == 200) {
-        final body = json.decode(response.body) as Map;
-        return MoviesResponse.fromJson(body as Map<String, dynamic>);
-      }
-      throw Exception('error fetching posts');
-    } catch (e) {
-      print('fetchPosts ERROR IS: $e');
-      throw Exception('error fetching posts');
+    final response = await httpClient.get(
+      Uri.https(
+        'live.mocat.amifactory.network',
+        '/api/v1/movies/',
+        <String, dynamic>{
+          'page': '$pageQuery',
+        },
+      ),
+    );
+    if (response.statusCode == 200) {
+      final body = json.decode(response.body) as Map;
+      return MoviesResponse.fromJson(body as Map<String, dynamic>);
     }
+    throw Exception('error fetching movies');
   }
 }
